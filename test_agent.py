dc1e62b4ce982ee8e2020c2648a4c22108ed081d
davoodya <davoodya40@gmail.com>
2025-07-06 20:19:25 +0330

    start Implementing test_processing_node() and add state: BusinessState Data

--- test_agent.py
+++ test_agent.py
@@ -1,5 +1,7 @@
+from turtledemo.penrose import start
+
 from schema import BusinessState
-from ai_agent import input_node
+from ai_agent import input_node, processing_node
 
 input_data = {
     "today": {
@@ -29,6 +31,17 @@ def test_input_node():
     result = input_node(state)
     assert result["input_data"]["today"]["revenue"] == 1200
     print("Input Node(1) Test passed!")
+
+def test_processing_node():
+    state: BusinessState = {
+        "input_data": {
+            "today": {"revenue": 1200, "cost": 800, "customers": 40},
+            "yesterday": {"revenue": 1000, "cost": 700, "customers": 35}
+        },
+        "metric": {},
+        "recommendation": ""
+    }
+
 
 if __name__ == "__main__":
     test_input_node()

64111407abb393413815eb575fd2262dc7a33c13
davoodya <davoodya40@gmail.com>
2025-07-06 20:15:05 +0330

    start defining processing_node() state 2.2

--- ai_agent.py
+++ ai_agent.py
@@ -5,3 +5,16 @@ from langgraph.graph import StateGraph
 def input_node(state: BusinessState) -> BusinessState:
     #state["input_data"] = input_data
     return state
+
+def processing_node(state: BusinessState) -> BusinessState:
+    # Define Datas
+    data = state["input_data"]
+
+    today = data["today"]
+    yesterday = data["yesterday"]
+
+    # Calculate metric
+    profit = today["revenue"] - today["cost"]
+
+    revenue_change_pct = ((today["revenue"] - yesterday["revenue"]) / yesterday["revenue"]) * 100
+    cost_change_pct = ((today["cost"] - yesterday["cost"]) / yesterday["cost"]) * 100

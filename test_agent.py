d0d3c23f309088fdac4837b55d814f02e4960eb2
davoodya <davoodya40@gmail.com>
2025-07-06 20:25:10 +0330

    add running node codes

--- test_agent.py
+++ test_agent.py
@@ -14,7 +14,7 @@ input_data = {
         "cost1": 700,
         "customers": 35
     },
-    "metrics": {},
+    "metric": {},
     "recommendation": ""
 }
 
@@ -42,6 +42,20 @@ def test_processing_node():
         "recommendation": ""
     }
 
+    # Running node
+    result = processing_node(state)
+    metric = result["metric"]
+
+    assert metric["profit"] == 400
+    assert round(metric["revenue_change_pct"], 1) == 20.0
+    assert round(metric["cost_change_pct"], 1) == 33.3
+    assert round(metric["today_cac"], 1) == 20.0
+    assert round(metric["yesterday_cac"], 1) == 20.0
+    assert round(metric["cac_change_pct"], 1) == 0.0
+
+    print("Processing Node(2) Test passed!")
+
 
 if __name__ == "__main__":
     test_input_node()
+    test_processing_node()

b29962ca12351125774b9c6f06f37b22f1ca2206
davoodya <davoodya40@gmail.com>
2025-07-06 21:32:52 +0330

    add test data

--- test_full_agent.py
+++ test_full_agent.py
@@ -1,3 +1,26 @@
 from ai_agent import build_agent
 from schema import BusinessState
+import math
+
+
+def test_full_agent():
+    agent = build_agent()
+
+    # add test data for a process all things say in resume
+    input_data: BusinessState = {
+        "input_data": {
+            "today": {
+                "revenue": 1500,
+                "cost": 1300,
+                "customers": 50
+            },
+            "yesterday": {
+                "revenue": 1000,
+                "cost": 900,
+                "customers": 45
+            }
+        },
+        "metrics": {},
+        "recommendations": ""
+    }
 

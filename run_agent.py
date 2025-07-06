d2f8d3a9148365dd7822aa8880020617dacf39d0
davoodya <davoodya40@gmail.com>
2025-07-06 21:17:12 +0330

    add create the agent and test data to run_agent.py

--- run_agent.py
+++ run_agent.py
@@ -2,3 +2,15 @@ from ai_agent import build_agent
 from schema import BusinessState
 import json
 
+# Create the agent
+agent = build_agent()
+
+# Test Data
+state: BusinessState = {
+    "input_data": {
+        "today": {"revenue": 1400, "cost": 1100, "customers": 50},
+        "yesterday": {"revenue": 1200, "cost": 900, "customers": 45}
+    },
+    "metrics": {},
+    "recommendations": ""
+}

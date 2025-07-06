73c9cefbb11287692af321a00706b4ac6d2d5566
davoodya <davoodya40@gmail.com>
2025-07-06 22:35:09 +0330

    add error handling in processing_node()

--- ai_agent.py
+++ ai_agent.py
@@ -7,33 +7,37 @@ def input_node(state: BusinessState) -> BusinessState:
     return state
 
 def processing_node(state: BusinessState) -> BusinessState:
-    # Define Datas
-    data = state["input_data"]
+    try:
+        # Define Datas
+        data = state["input_data"]
 
-    today = data["today"]
-    yesterday = data["yesterday"]
+        today = data["today"]
+        yesterday = data["yesterday"]
 
-    # Calculate metrics
-    profit = today["revenue"] - today["cost"]
+        # Calculate metrics
+        profit = today["revenue"] - today["cost"]
 
-    revenue_change_pct = ((today["revenue"] - yesterday["revenue"]) / yesterday["revenue"]) * 100
-    cost_change_pct = ((today["cost"] - yesterday["cost"]) / yesterday["cost"]) * 100
+        revenue_change_pct = ((today["revenue"] - yesterday["revenue"]) / yesterday["revenue"]) * 100
+        cost_change_pct = ((today["cost"] - yesterday["cost"]) / yesterday["cost"]) * 100
 
-    today_cac = today["cost"] / today["customers"] if today["customers"] else 0
-    yesterday_cac = yesterday["cost"] / yesterday["customers"] if yesterday["customers"] else 0
-    cac_change_pct = ((today_cac - yesterday_cac) / yesterday_cac) * 100 if yesterday_cac else 0
+        today_cac = today["cost"] / today["customers"] if today["customers"] else 0
+        yesterday_cac = yesterday["cost"] / yesterday["customers"] if yesterday["customers"] else 0
+        cac_change_pct = ((today_cac - yesterday_cac) / yesterday_cac) * 100 if yesterday_cac else 0
 
-    # Save in State
-    state["metrics"] = {
-        "profit": profit,
-        "revenue_change_pct": revenue_change_pct,
-        "cost_change_pct": cost_change_pct,
-        "today_cac": today_cac,
-        "yesterday_cac": yesterday_cac,
-        "cac_change_pct": cac_change_pct
-    }
+        # Save in State
+        state["metrics"] = {
+            "profit": profit,
+            "revenue_change_pct": revenue_change_pct,
+            "cost_change_pct": cost_change_pct,
+            "today_cac": today_cac,
+            "yesterday_cac": yesterday_cac,
+            "cac_change_pct": cac_change_pct
+        }
 
-    return state
+        return state
+    except Exception as e:
+        state["metrics"] = {"error": str(e)}
+        return state
 
 def recommendation_node(state: BusinessState) -> BusinessState:
     metrics = state["metrics"]

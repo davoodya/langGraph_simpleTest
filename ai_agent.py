8ffbfd7344f2623ff9cb303ef297942ac4483b91
davoodya <davoodya40@gmail.com>
2025-07-06 21:26:54 +0330

    add new lines to recommendation_node()

--- ai_agent.py
+++ ai_agent.py
@@ -41,15 +41,15 @@ def recommendation_node(state: BusinessState) -> BusinessState:
 
     # 1. Profit Checking
     if metrics["profit"] < 0:
-        messages.append("Profit is negative. Consider reducing operational costs.")
+        messages.append("Profit is negative.\n Consider reducing operational costs.")
 
     # 2. CAC growing checking
     if metrics["cac_change_pct"] > 20:
-        messages.append("CAC has increased significantly. "
+        messages.append("CAC has increased significantly.\n "
                         "Invest in customer acquisition strategies or Review marketing campaigns .")
     # 3. Sales Growing checking
     if metrics["revenue_change_pct"] > 0:
-        messages.append("Sales have grown, Consider increasing advertising budget.")
+        messages.append("Sales have grown.\n Consider increasing advertising budget.")
 
     # 4. if Not messages means all metrics is OK
     if not messages:

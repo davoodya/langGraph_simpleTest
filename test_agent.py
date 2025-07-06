1fec6da579bc2d923a986d5a39e54641a7d6b08c
davoodya <davoodya40@gmail.com>
2025-07-06 20:10:37 +0330

    add test_agent.py to testing ai agent

--- test_agent.py
+++ test_agent.py
@@ -16,3 +16,19 @@ input_data = {
     "recommendation": ""
 }
 
+def test_input_node():
+    state: BusinessState = {
+        "input_data": {
+            "today": {"revenue": 1200, "cost": 800, "customers": 40},
+            "yesterday": {"revenue": 1000, "cost": 700, "customers": 35}
+        },
+        "metric": {},
+        "recommendation": ""
+    }
+
+    result = input_node(state)
+    assert result["input_data"]["today"]["revenue"] == 1200
+    print("Input Node(1) Test passed!")
+
+if __name__ == "__main__":
+    test_input_node()
